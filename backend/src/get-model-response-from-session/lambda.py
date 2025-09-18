b223861dd3656911297c7335cdafeb27efeb2ccc
Sebastian Wegener <mail@wegener-sebastian.de>
2025-09-18 13:04:12 +0200

    [AIInform] - changed REST paths and added some lambda print statements

--- backend/src/get-model-response-from-session/lambda.py
+++ backend/src/get-model-response-from-session/lambda.py
@@ -2,5 +2,6 @@ import json
 def handler(event, context):
     # Log the event argument for debugging and for use in local development.
     print(json.dumps(event))
+    print("get model response from session lambda")
 
     return {}
--- backend/src/get-text-from-image/lambda.py
+++ backend/src/get-text-from-image/lambda.py
@@ -2,5 +2,6 @@ import json
 def handler(event, context):
     # Log the event argument for debugging and for use in local development.
     print(json.dumps(event))
+    print("get text from image lambda")
 
     return {}
