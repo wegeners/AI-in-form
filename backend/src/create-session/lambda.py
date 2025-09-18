35274405e23d27a8c4681e9d3f664b73e93fecdf
TheFish <finnwfreeman@icloud.com>
2025-09-18 17:40:13 +0200

    Add session creation functionality in create-session lambda
    Implemented logic to store session data in DynamoDB, including session ID and associated image. Enhanced the handler to log incoming events for debugging purposes.

--- backend/src/create-session/lambda.py
+++ backend/src/create-session/lambda.py
@@ -1,6 +1,24 @@
 import json
+import os
+import boto3
+
 def handler(event, context):
     # Log the event argument for debugging and for use in local development.
     print(json.dumps(event))
 
-    return {}
+    table_name = os.environ['FORMSESSION_TABLE_NAME']
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table(table_name)
+
+    session_id = event['sessionId']
+    image = event['image']
+
+    table.put_item(
+        Item={
+            'sessionId': session_id,
+            'step': 0,
+            'image': image,
+        }
+    )
+
+    return event
