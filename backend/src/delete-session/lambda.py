ddfa866e4fe13c4597474cf0a2b1c9a61f3d01e2
Sebastian Wegener <mail@wegener-sebastian.de>
2025-09-18 17:24:39 +0200

    [AIInform] - commited changes for delete session lambda

--- backend/src/delete-session/lambda.py
+++ backend/src/delete-session/lambda.py
@@ -5,6 +5,7 @@ from botocore.exceptions import ClientError
 from boto3.dynamodb.conditions import Key
 
 def handler(event, context):
+    print(event)
     session_id = event.get("session_id")
     if not session_id:
         return {"error": "Missing session_id"}
@@ -24,19 +25,19 @@ def handler(event, context):
         while True:
             if last_evaluated_key:
                 response = table.query(
-                    KeyConditionExpression=Key("session_id").eq(session_id),
+                    KeyConditionExpression=Key("sessionId").eq(session_id),
                     ExclusiveStartKey=last_evaluated_key,
                 )
             else:
                 response = table.query(
-                    KeyConditionExpression=Key("session_id").eq(session_id)
+                    KeyConditionExpression=Key("sessionId").eq(session_id)
                 )
             items = response.get("Items", [])
 
             with table.batch_writer() as batch:
                 for item in items:
                     key = {
-                        "session_id": session_id,
+                        "sessionId": session_id,
                         "step": item["step"]
                     }
                     batch.delete_item(Key=key)
@@ -46,7 +47,7 @@ def handler(event, context):
             if not last_evaluated_key:
                 break
 
-        return {"deleted": deleted_count, "session_id": session_id}
+        return {"deleted": deleted_count, "sessionId": session_id}
     except ClientError as e:
         return {"error": e.response['Error'].get('Message', str(e))}
     except Exception as e:
