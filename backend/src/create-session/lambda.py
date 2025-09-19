d5c6cb592626a98dc476f547186762a012853d53
Sebastian Wegener <mail@wegener-sebastian.de>
2025-09-19 09:31:42 +0200

    [AIInform] - changed create session lambda

--- backend/src/create-session/lambda.py
+++ backend/src/create-session/lambda.py
@@ -4,20 +4,23 @@ import boto3
 
 def handler(event, context):
     # Log the event argument for debugging and for use in local development.
+    print("this is the event")
     print(json.dumps(event))
 
     table_name = os.environ['FORMSESSION_TABLE_NAME']
     dynamodb = boto3.resource('dynamodb')
     table = dynamodb.Table(table_name)
 
-    session_id = event['sessionId']
-    image = event['image']
+    session_id = event["sessionId"]
+    #image = event['image']
+    user_question = event["userQuestion"]
 
     table.put_item(
         Item={
             'sessionId': session_id,
             'step': 0,
-            'image': image,
+            #'image': image,
+            'value': user_question
         }
     )
 
